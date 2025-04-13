f046a49704c80cab5b87a26070bc738347f01b80
umadevi-12 <bogathiu@gmail.com>
2025-04-13 12:28:53 +0530

    done 3

---
+++ problem3.js
@@ -0,0 +1,19 @@
+function encryptIt(S,K) {
+    // write code here  
+    let bag = ""
+    for(let i=0;i<S.length;i++){
+      let char = S[i];
+      
+    }
+    if(char >= 'A' && char <= 'Z'){
+      else if(char >= 'a' && char <='z'){
+        else if(char >='0' && char <= '9'){
+          else{
+            result += char;
+          }
+        }
+      }
+      
+    }
+    console.log(result)
+}
